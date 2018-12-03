873a7d9e5d8a065885ab76307cdc5a32b7449e78
hanerx <zhangyu199946@gmail.com>
2018-12-03 19:54:51 +0800

    vue初始化

---
+++ Application/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+        <v-app>
+            <v-navigation-drawer app></v-navigation-drawer>
+            <v-toolbar app></v-toolbar>
+            <v-content>
+                <v-container fluid>
+                    <login></login>
+                </v-container>
+            </v-content>
+            <v-footer app></v-footer>
+        </v-app>
+    </div>
+</template>
+
+<script>
+    import HelloWorld from './components/HelloWorld.vue'
+    import Login from "./components/Login";
+
+    export default {
+        name: 'app',
+        components: {
+            Login,
+            HelloWorld
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
