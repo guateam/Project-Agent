06437f4e6ecc8b0c714f7e9dca80648dfb239ac3
laipz8200 <laipz8200@icloud.com>
2018-12-07 11:05:56 +0800

    add router

--- Application/src/main.js
+++ Application/src/main.js
@@ -2,12 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+
 // index.js or main.js
 import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+
+import router from './router'
 
 Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
