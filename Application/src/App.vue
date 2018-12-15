88aa4b3de919104740588f29849d234fa8a76a6c
laipz8200 <laipz8200@icloud.com>
2018-12-15 14:15:18 +0800

    update
    1. 添加了登录和注册的数据绑定
    2. 添加了登录的接口参数传递
    3. 添加了主页下方导航
    4. 添加了注册和登录还有返回首页的路由
    5. 修改了首页卡片样式

--- Application/src/App.vue
+++ Application/src/App.vue
@@ -2,6 +2,41 @@
   <div id="app">
     <Myheader></Myheader>
     <router-view></router-view>
+    <v-bottom-nav
+            :active.sync="bottomNav"
+            :value="true"
+            absolute
+            color="transparent"
+    >
+      <v-btn
+              color="teal"
+              flat
+              value="home"
+              @click="$router.push('/home')"
+      >
+        <span>首页</span>
+        <v-icon>web</v-icon>
+      </v-btn>
+
+      <v-btn
+              color="teal"
+              flat
+              value="message"
+      >
+        <span>消息</span>
+        <v-icon>question_answer</v-icon>
+      </v-btn>
+
+      <v-btn
+              color="teal"
+              flat
+              value="mine"
+              @click="$router.push('/login')"
+      >
+        <span>我的</span>
+        <v-icon>account_circle</v-icon>
+      </v-btn>
+    </v-bottom-nav>
   </div>
 </template>
 
