76f57b20604fe0af400d8e0736ea0ad606e46e9f
Asgardusk <13979215061@163.com>
2018-12-17 20:48:39 +0800

    重构了一下消息页

--- Application/src/components/home/home.vue
+++ Application/src/components/home/home.vue
@@ -1,131 +1,128 @@
 <template>
     <div class="home">
-        <div>
-            <v-toolbar color="#0065cc" dark tabs style="padding-top: 1em">
-                <v-text-field append-icon="mic" class="mx-3" flat label="Search" prepend-inner-icon="search"
-                              solo-inverted></v-text-field>
-                <v-tabs slot="extension" v-model="tabs" centered color="transparent" slider-color="white">
-                    <v-tab :key="1">推荐</v-tab>
-                    <v-tab :key="2">计算机</v-tab>
-                    <v-tab :key="3">互联网</v-tab>
-                    <v-tab :key="4">通信</v-tab>
-                    <v-tab :key="5">信息安全</v-tab>
-                </v-tabs>
-            </v-toolbar>
+        <v-toolbar color="#0065cc" dark tabs style="padding-top: 1em">
+            <v-text-field append-icon="mic" class="mx-3" flat label="Search" prepend-inner-icon="search" solo-inverted></v-text-field>
+            <v-tabs slot="extension" v-model="tabs" centered color="transparent"slider-color="white">
+                <v-tab :key="1">推荐</v-tab>
+                <v-tab :key="2">计算机</v-tab>
+                <v-tab :key="3">互联网</v-tab>
+                <v-tab :key="4">通信</v-tab>
+                <v-tab :key="5">信息安全</v-tab>
+            </v-tabs>
+        </v-toolbar>
 
-            <v-tabs-items v-model="tabs">
-                <v-tab-item :key="1">
-                    <v-card color="white" style="margin-top: 1em" @click="$router.push('/topic')">
-                        <v-container grid-list-md text-xs-center>
-                            <v-layout row wrap>
-                                <v-flex xs7 style="text-align: left">
-                                    <h3>{{title}}</h3>
-                                </v-flex>
-                                <v-flex xs5>
-                                    <img src="./1.png" style="width: 100%">
-                                </v-flex>
-                                <div style="display: flex;width: 100%;text-align: left;justify-content: space-between;">
-                                    <div>
-                                        <p>标签&nbsp;&nbsp;·&nbsp;&nbsp;<span>科技/人工智能</span></p>
-                                        <p><span>4342</span>人关注&nbsp;&nbsp;&nbsp;·&nbsp;&nbsp;&nbsp;<span>40</span>条评论
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p style="margin-top: 1em">12小时前</p>
-                                    </div>
+        <v-tabs-items v-model="tabs">
+            <v-tab-item :key="1">
+                <v-card color="white" style="margin-top: 1em" @click="$router.push('/topic')">
+                    <v-container grid-list-md text-xs-center>
+                        <v-layout row wrap>
+                            <v-flex xs7 style="text-align: left">
+                                <h3>{{title}}</h3>
+                            </v-flex>
+                            <v-flex xs5>
+                                <img src="./1.png" style="width: 100%">
+                            </v-flex>
+                            <div style="display: flex;width: 100%;text-align: left;justify-content: space-between;">
+                                <div>
+                                    <p>标签&nbsp;&nbsp;·&nbsp;&nbsp;<span>科技/人工智能</span></p>
+                                    <p><span>4342</span>人关注&nbsp;&nbsp;&nbsp;·&nbsp;&nbsp;&nbsp;<span>40</span>条评论
+                                    </p>
                                 </div>
-                            </v-layout>
-                        </v-container>
-                    </v-card>
-
-                    <!--以下为复制的内容，不用管了。可以直接删-->
-
-                    <v-card color="white" style="margin-top: 1em" @click="$router.push('/topic')">
-                        <v-container grid-list-md text-xs-center>
-                            <v-layout row wrap>
-                                <v-flex xs7 style="text-align: left">
-                                    <h3>{{title}}</h3>
-                                </v-flex>
-                                <v-flex xs5>
-                                    <img src="./1.png" style="width: 100%">
-                                </v-flex>
-                                <div style="display: flex;width: 100%;text-align: left;justify-content: space-between;">
-                                    <div>
-                                        <p>标签&nbsp;&nbsp;·&nbsp;&nbsp;<span>科技/人工智能</span></p>
-                                        <p><span>4342</span>人关注&nbsp;&nbsp;&nbsp;·&nbsp;&nbsp;&nbsp;<span>40</span>条评论
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p style="margin-top: 1em">12小时前</p>
-                                    </div>
+                                <div>
+                                    <p style="margin-top: 1em">12小时前</p>
                                 </div>
-                            </v-layout>
-                        </v-container>
-                    </v-card>
-                    <v-card color="white" style="margin-top: 1em" @click="$router.push('/topic')">
-                        <v-container grid-list-md text-xs-center>
-                            <v-layout row wrap>
-                                <v-flex xs7 style="text-align: left">
-                                    <h3>{{title}}</h3>
-                                </v-flex>
-                                <v-flex xs5>
-                                    <img src="./1.png" style="width: 100%">
-                                </v-flex>
-                                <div style="display: flex;width: 100%;text-align: left;justify-content: space-between;">
-                                    <div>
-                                        <p>标签&nbsp;&nbsp;·&nbsp;&nbsp;<span>科技/人工智能</span></p>
-                                        <p><span>4342</span>人关注&nbsp;&nbsp;&nbsp;·&nbsp;&nbsp;&nbsp;<span>40</span>条评论
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p style="margin-top: 1em">12小时前</p>
-                                    </div>
+                            </div>
+                        </v-layout>
+                    </v-container>
+                </v-card>
+
+                <!--以下为复制的内容，不用管了。可以直接删-->
+
+                <v-card color="white" style="margin-top: 1em" @click="$router.push('/topic')">
+                    <v-container grid-list-md text-xs-center>
+                        <v-layout row wrap>
+                            <v-flex xs7 style="text-align: left">
+                                <h3>{{title}}</h3>
+                            </v-flex>
+                            <v-flex xs5>
+                                <img src="./1.png" style="width: 100%">
+                            </v-flex>
+                            <div style="display: flex;width: 100%;text-align: left;justify-content: space-between;">
+                                <div>
+                                    <p>标签&nbsp;&nbsp;·&nbsp;&nbsp;<span>科技/人工智能</span></p>
+                                    <p><span>4342</span>人关注&nbsp;&nbsp;&nbsp;·&nbsp;&nbsp;&nbsp;<span>40</span>条评论
+                                    </p>
                                 </div>
-                            </v-layout>
-                        </v-container>
-                    </v-card>
-                    <v-card color="white" style="margin-top: 1em" @click="$router.push('/topic')">
-                        <v-container grid-list-md text-xs-center>
-                            <v-layout row wrap>
-                                <v-flex xs7 style="text-align: left">
-                                    <h3>{{title}}</h3>
-                                </v-flex>
-                                <v-flex xs5>
-                                    <img src="./1.png" style="width: 100%">
-                                </v-flex>
-                                <div style="display: flex;width: 100%;text-align: left;justify-content: space-between;">
-                                    <div>
-                                        <p>标签&nbsp;&nbsp;·&nbsp;&nbsp;<span>科技/人工智能</span></p>
-                                        <p><span>4342</span>人关注&nbsp;&nbsp;&nbsp;·&nbsp;&nbsp;&nbsp;<span>40</span>条评论
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p style="margin-top: 1em">12小时前</p>
-                                    </div>
+                                <div>
+                                    <p style="margin-top: 1em">12小时前</p>
                                 </div>
-                            </v-layout>
-                        </v-container>
-                    </v-card>
+                            </div>
+                        </v-layout>
+                    </v-container>
+                </v-card>
+                <v-card color="white" style="margin-top: 1em" @click="$router.push('/topic')">
+                    <v-container grid-list-md text-xs-center>
+                        <v-layout row wrap>
+                            <v-flex xs7 style="text-align: left">
+                                <h3>{{title}}</h3>
+                            </v-flex>
+                            <v-flex xs5>
+                                <img src="./1.png" style="width: 100%">
+                            </v-flex>
+                            <div style="display: flex;width: 100%;text-align: left;justify-content: space-between;">
+                                <div>
+                                    <p>标签&nbsp;&nbsp;·&nbsp;&nbsp;<span>科技/人工智能</span></p>
+                                    <p><span>4342</span>人关注&nbsp;&nbsp;&nbsp;·&nbsp;&nbsp;&nbsp;<span>40</span>条评论
+                                    </p>
+                                </div>
+                                <div>
+                                    <p style="margin-top: 1em">12小时前</p>
+                                </div>
+                            </div>
+                        </v-layout>
+                    </v-container>
+                </v-card>
+                <v-card color="white" style="margin-top: 1em" @click="$router.push('/topic')">
+                    <v-container grid-list-md text-xs-center>
+                        <v-layout row wrap>
+                            <v-flex xs7 style="text-align: left">
+                                <h3>{{title}}</h3>
+                            </v-flex>
+                            <v-flex xs5>
+                                <img src="./1.png" style="width: 100%">
+                            </v-flex>
+                            <div style="display: flex;width: 100%;text-align: left;justify-content: space-between;">
+                                <div>
+                                    <p>标签&nbsp;&nbsp;·&nbsp;&nbsp;<span>科技/人工智能</span></p>
+                                    <p><span>4342</span>人关注&nbsp;&nbsp;&nbsp;·&nbsp;&nbsp;&nbsp;<span>40</span>条评论
+                                    </p>
+                                </div>
+                                <div>
+                                    <p style="margin-top: 1em">12小时前</p>
+                                </div>
+                            </div>
+                        </v-layout>
+                    </v-container>
+                </v-card>
 
 
-                    <!--复制结束，下面的items都是空的，直接把最上面的v-card搬过来就OK-->
+                <!--复制结束，下面的items都是空的，直接把最上面的v-card搬过来就OK-->
 
 
-                </v-tab-item>
-                <v-tab-item :key="2">
-                    22222
-                </v-tab-item>
-                <v-tab-item :key="3">
-                    33333
-                </v-tab-item>
-                <v-tab-item :key="4">
-                    44444
-                </v-tab-item>
-                <v-tab-item :key="5">
-                    55555
-                </v-tab-item>
-            </v-tabs-items>
-        </div>
+            </v-tab-item>
+            <v-tab-item :key="2">
+                22222
+            </v-tab-item>
+            <v-tab-item :key="3">
+                33333
+            </v-tab-item>
+            <v-tab-item :key="4">
+                44444
+            </v-tab-item>
+            <v-tab-item :key="5">
+                55555
+            </v-tab-item>
+        </v-tabs-items>
     </div>
 </template>
 
@@ -160,9 +157,10 @@
 </script>
 
 <style scoped>
-    p{
+    p {
         margin-bottom: 0;
     }
+
     .head {
         width: 100%;
         height: 8em;
--- Application/src/router/router.js
+++ Application/src/router/router.js
@@ -8,6 +8,7 @@ import AnswerDetail from '../components/answer-detail/answer-detail'
 import Comment from '../components/comment/comment'
 import Message from '../components/message/message'
 import School from '../components/school/school'
+import Chat from '../components/chat/chat'
 
 Vue.use(Router);
 
@@ -68,6 +69,12 @@ export default new Router({
             path: '/message',
             name: 'message',
             component: Message,
-        }
+            children: [
+            ]
+        },{
+            path: '/chat',
+            name: 'chat',
+            component: Chat,
+        },
     ]
 })
