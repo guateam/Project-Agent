fefefe1bfb83e29fa32662c90a6b28b4f2e3b5ff
yun_cn <zzwyxl@163.com>
2019-01-25 19:02:01 +0800

    模糊搜索算法+数据库更新+db.py新增函数

--- API/db.py
+++ API/db.py
@@ -226,3 +226,41 @@ class Database(object):
         except pymysql.mySQLError as e:
             print(e.args)
             return []
+
+    def vague(self, data, table, type=0):
+        """
+        更加模糊的搜索
+        :param data: 模糊搜索数据
+        :param table: 目标表名
+        :return:
+        """
+        try:
+            with self.db.cursor() as cursor:
+                if not data:  # 判断data是否为空
+                    return self.get(data, table)
+                list1 = []
+                for key, values in data.items():
+                    if values == self.MYSQL_NULL:
+                        list1.append(key + self.MYSQL_NULL)
+                    else:
+                        if isinstance(values, str):
+                            str_use = ''
+                            if len(list1) == 0:
+                                str_use = (key + ' LIKE \'%')
+                            for single in values:
+                                str_use += single + '%'
+                            str_use +='\''
+                            list1.append(str_use)
+                        else:
+                            list1.append(key + ' LIKE \'%' + str(values) + '%\'')
+
+                where = ' AND '.join(list1)
+                sql_query = 'SELECT * FROM %s WHERE %s' % (table, where)  # 构造sql语句
+                cursor.execute(sql_query)
+                results = cursor.fetchall()
+                if len(results) == 1 and type == 1:
+                    return results[0]  # 返回单个数组
+                return results  # 返回多个数组
+        except pymysql.MySQLError as e:
+            print(e.args)
+            return []  # 报错
