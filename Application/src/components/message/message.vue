55dc3e985e4e091da057e7afa4569aad14b379f3
Kffhi <13979215061@163.com>
2019-02-24 15:41:13 +0800

    修复bug

--- Application/src/components/message/message.vue
+++ Application/src/components/message/message.vue
@@ -1,129 +1,89 @@
 <template>
     <div class="message">
+        <div>
+            <v-layout row>
+                <v-flex xs12 offset-sm3>
+                    <v-card>
+                        <v-toolbar color="white" tabs>
+                            <v-toolbar-title style="width: 100%; text-align: center;">我的消息</v-toolbar-title>
+                            <v-tabs slot="extension" v-model="tabs" centered color="white" slider-color="#FFCC00">
+                                <v-tab :key="1">私聊</v-tab>
+                                <v-tab :key="2">好友</v-tab>
+                                <v-tab :key="3">通知</v-tab>
+                                <v-tab :key="4">群组</v-tab>
+                            </v-tabs>
+                        </v-toolbar>
+                    </v-card>
+                </v-flex>
+            </v-layout>
 
-        <v-layout row>
-            <v-flex xs12 offset-sm3>
-                <v-card>
-                    <v-toolbar color="white" tabs>
-                        <v-toolbar-title style="width: 100%; text-align: center;">我的消息</v-toolbar-title>
-                        <v-tabs slot="extension" v-model="tabs" centered color="white" slider-color="#FFCC00">
-                            <v-tab :key="1">私聊</v-tab>
-                            <v-tab :key="2">好友</v-tab>
-                            <v-tab :key="3">通知</v-tab>
-                            <v-tab :key="4">群组</v-tab>
-                        </v-tabs>
-                    </v-toolbar>
-                </v-card>
-            </v-flex>
-        </v-layout>
+            <div style="margin-top: 1em;"></div>
 
-        <div style="margin-top: 1em;"></div>
-
-        <v-tabs-items v-model="tabs">
-            <v-tab-item :key="1">
-                <v-card>
-                    <v-list two-line>
-                        <template v-for="(item, index) in items1">
-                            <v-subheader v-if="item.header" :key="item.header">
-                                {{ item.header }}
-                            </v-subheader>
-                            <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
-                            <v-list-tile v-else :key="item.title" avatar @click="$router.push('./chat')">
-                                <v-list-tile-avatar>
-                                    <img :src="item.avatar">
-                                </v-list-tile-avatar>
-                                <v-list-tile-content>
-                                    <v-list-tile-title v-html="item.title"></v-list-tile-title>
-                                    <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
-                                </v-list-tile-content>
-                            </v-list-tile>
-                        </template>
-                    </v-list>
-                </v-card>
+            <v-tabs-items v-model="tabs">
+                <v-tab-item :key="1">
+                    <v-card>
+                        <v-list two-line>
+                            <template v-for="(item, index) in items1">
+                                <v-subheader v-if="item.header" :key="item.header">
+                                    {{ item.header }}
+                                </v-subheader>
+                                <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
+                                <v-list-tile v-else :key="item.title" avatar @click="$router.push('./chat')">
+                                    <v-list-tile-avatar>
+                                        <img :src="item.avatar">
+                                    </v-list-tile-avatar>
+                                    <v-list-tile-content>
+                                        <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                                        <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                                    </v-list-tile-content>
+                                </v-list-tile>
+                            </template>
+                        </v-list>
+                    </v-card>
 
 
-            </v-tab-item>
-            <v-tab-item :key="2">
-                <v-list>
-                    <v-list-group
-                            v-for="item in friendList"
-                            v-model="item.active"
-                            :key="item.title"
-                            no-action
-                    >
-                        <!--:prepend-icon="item.action"-->
-                        <v-list-tile slot="activator">
-                            <v-list-tile-content>
-                                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-
-                        <v-list-tile
-                                v-for="subItem in item.items"
-                                :key="subItem.nickname"
+                </v-tab-item>
+                <v-tab-item :key="2">
+                    <v-list>
+                        <v-list-group
+                                v-for="item in friendList"
+                                v-model="item.active"
+                                :key="item.title"
+                                no-action
                         >
-                            <v-list-tile-avatar>
-                                <img :src="subItem.headportrait">
-                            </v-list-tile-avatar>
-                            <v-list-tile-content>
-                                <v-list-tile-title>{{ subItem.nickname }}</v-list-tile-title>
-                            </v-list-tile-content>
+                            <!--:prepend-icon="item.action"-->
+                            <v-list-tile slot="activator">
+                                <v-list-tile-content>
+                                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
 
-                            <!--<v-list-tile-action>-->
-                            <!--<v-icon>{{ subItem.action }}</v-icon>-->
-                            <!--</v-list-tile-action>-->
-                        </v-list-tile>
-                    </v-list-group>
-                </v-list>
-            </v-tab-item>
-            <v-tab-item :key="3">
-                <v-list two-line>
-                    <template v-for="(item, index) in items3">
-                        <v-subheader v-if="item.header" :key="item.header">
-                            {{ item.header }}
-                        </v-subheader>
-                        <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
-                        <v-list-tile v-else :key="item.title" avatar @click="$router.push('./approval')">
-                            <v-list-tile-avatar>
-                                <img :src="item.avatar">
-                            </v-list-tile-avatar>
-                            <v-list-tile-content>
-                                <v-list-tile-title v-html="item.title"></v-list-tile-title>
-                                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                    </template>
-                </v-list>
+                            <v-list-tile
+                                    v-for="subItem in item.items"
+                                    :key="subItem.nickname"
+                            >
+                                <v-list-tile-avatar>
+                                    <img :src="subItem.headportrait">
+                                </v-list-tile-avatar>
+                                <v-list-tile-content>
+                                    <v-list-tile-title>{{ subItem.nickname }}</v-list-tile-title>
+                                </v-list-tile-content>
 
-                <v-divider></v-divider>
-
-                <v-list two-line>
-                    <template v-for="(item, index) in items4">
-                        <v-subheader v-if="item.header" :key="item.header">
-                            {{ item.header }}
-                        </v-subheader>
-                        <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
-                        <v-list-tile v-else :key="item.title" avatar @click="$router.push('./callme')">
-                            <v-list-tile-avatar>
-                                <img :src="item.avatar">
-                            </v-list-tile-avatar>
-                            <v-list-tile-content>
-                                <v-list-tile-title v-html="item.title"></v-list-tile-title>
-                                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                    </template>
-                </v-list>
-            </v-tab-item>
-            <v-tab-item :key="4">
-                <v-card>
+                                <!--<v-list-tile-action>-->
+                                <!--<v-icon>{{ subItem.action }}</v-icon>-->
+                                <!--</v-list-tile-action>-->
+                            </v-list-tile>
+                        </v-list-group>
+                    </v-list>
+                </v-tab-item>
+                <v-tab-item :key="3">
                     <v-list two-line>
-                        <template v-for="(item, index) in items1">
+                        <template v-for="(item, index) in items3">
                             <v-subheader v-if="item.header" :key="item.header">
                                 {{ item.header }}
                             </v-subheader>
                             <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
-                            <v-list-tile v-else :key="item.title" avatar @click="$router.push('./chat')">
+                            <v-list-tile v-else :key="item.title" avatar @click="$router.push('./approval')">
                                 <v-list-tile-avatar>
                                     <img :src="item.avatar">
                                 </v-list-tile-avatar>
@@ -134,10 +94,51 @@
                             </v-list-tile>
                         </template>
                     </v-list>
-                </v-card>
-            </v-tab-item>
-        </v-tabs-items>
 
+                    <v-divider></v-divider>
+
+                    <v-list two-line>
+                        <template v-for="(item, index) in items4">
+                            <v-subheader v-if="item.header" :key="item.header">
+                                {{ item.header }}
+                            </v-subheader>
+                            <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
+                            <v-list-tile v-else :key="item.title" avatar @click="$router.push('./callme')">
+                                <v-list-tile-avatar>
+                                    <img :src="item.avatar">
+                                </v-list-tile-avatar>
+                                <v-list-tile-content>
+                                    <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                                    <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                        </template>
+                    </v-list>
+                </v-tab-item>
+                <v-tab-item :key="4">
+                    <v-card>
+                        <v-list two-line>
+                            <template v-for="(item, index) in items1">
+                                <v-subheader v-if="item.header" :key="item.header">
+                                    {{ item.header }}
+                                </v-subheader>
+                                <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
+                                <v-list-tile v-else :key="item.title" avatar @click="$router.push('./chat')">
+                                    <v-list-tile-avatar>
+                                        <img :src="item.avatar">
+                                    </v-list-tile-avatar>
+                                    <v-list-tile-content>
+                                        <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                                        <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                                    </v-list-tile-content>
+                                </v-list-tile>
+                            </template>
+                        </v-list>
+                    </v-card>
+                </v-tab-item>
+            </v-tabs-items>
+        </div>
+        <router-view></router-view>
     </div>
 </template>
 
@@ -240,7 +241,7 @@
                 // 获取好友列表
                 import('js-cookie').then((Cookies) => {
                     import('axios').then((axios) => {
-                        axios.get('https://'+this.GLOBAL.host+'/api/message/get_friend_list', {
+                        axios.get('https://' + this.GLOBAL.host + '/api/message/get_friend_list', {
                             responseType: 'json',
                             params: {
                                 token: this.GLOBAL.token,
@@ -255,7 +256,7 @@
             get_message_list() {
                 import('js-cookie').then((Cookies) => {
                     import('axios').then((axios) => {
-                        axios.get('https://'+this.GLOBAL.host+'/api/message/get_message_list', {
+                        axios.get('https://' + this.GLOBAL.host + '/api/message/get_message_list', {
                             responseType: 'json',
                             params: {
                                 token: this.GLOBAL.token,
